198019566555682948d70436875305c3b3f8ed72
Cory Coward <ccoward80@gmail.com>
2025-05-18 18:47:06 -0400

    Chapter three

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from typing import Any
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -17,3 +18,11 @@ def index() -> str:
         },
     ]
     return render_template("index.html.j2", title="Home", user=user, posts=posts)
+
+@app.route("/login", methods=["GET", "POST"])
+def login() -> str:
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for user {form.username.data}, remember_me={form.remember_me.data}.")
+        return redirect(url_for("index"))
+    return render_template("login.html.j2", title="Sign In", form=form)
