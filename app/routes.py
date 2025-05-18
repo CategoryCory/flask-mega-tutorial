8671159bea27f2009397ea98b2237493ab8622d7
Cory Coward <ccoward80@gmail.com>
2025-05-18 14:14:26 -0400

    Chapter two

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
+from typing import Any
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
-def index():
-    return "Hello world!"
+def index() -> str:
+    user: dict[str, str] = {"username": "Bob"}
+    posts: list[dict[str, Any]] = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Portland!",
+        },
+        {
+            "author": {"username": "Mark"},
+            "body": "Star Wars is awesome!",
+        },
+    ]
+    return render_template("index.html.j2", title="Home", user=user, posts=posts)
