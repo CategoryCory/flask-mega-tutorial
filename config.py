06128e2d1e4cee07239eee7e61be2130deaf0008
Cory Coward <ccoward80@gmail.com>
2025-05-18 22:17:05 -0400

    Chapter four

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "super-secret-key"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}"
